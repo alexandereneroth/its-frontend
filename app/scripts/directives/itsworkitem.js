ec86ab0cc00a25a548c80fe17d98f7ecf39b5f7a
alexander-eneroth <89alexius@gmail.com>
2015-10-02 16:00:53 +0200

    feat: puts workItem html model in a custom directive

---
+++ app/scripts/directives/itsworkitem.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name itsFrontendApp.directive:itsWorkItem
+ * @description
+ * # itsWorkItem
+ */
+angular.module('itsFrontendApp')
+	.directive('itsWorkItem', function () {
+		return { /*TODO change this URL*/
+			templateUrl: 'scripts/directives/itsworkitemtmpl.html',
+			restrict: 'E',
+			scope: {
+				workItem: '=model'
+
+			},
+			link: function postLink(scope, element, attrs) {}
+		};
+	});
